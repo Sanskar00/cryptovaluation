06a8b75bc8743e5a29372aeb42260c7dfac6e632
Sanskar00 <saxenasanskar090901@gmail.com>
2021-06-01 19:37:43 +0530

    bootstrap remove from navbar

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './searchField.css'
 import {Link} from 'react-router-dom';
 
