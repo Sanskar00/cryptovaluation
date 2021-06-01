e88741bac3e121f4efa19b233f2445f19aeedba5
Sanskar00 <66662650+Sanskar00@users.noreply.github.com>
2021-06-02 01:23:22 +0530

    Update coinList.js

--- src/component/CoinLIst/coinLIst/coinList.js
+++ src/component/CoinLIst/coinLIst/coinList.js
@@ -1,6 +1,7 @@
 import React ,{useState,useEffect} from 'react';
 import { Table } from "react-bootstrap";
 import './coinList.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
 import { Navbar } from '../../navbar/navbar';
 
