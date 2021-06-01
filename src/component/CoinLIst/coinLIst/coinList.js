933170026834e5c478d9f84080f1981132da160e
Sanskar00 <66662650+Sanskar00@users.noreply.github.com>
2021-06-01 19:44:32 +0530

    Update coinList.js
    bootstrap removed from coinList.js

--- src/component/CoinLIst/coinLIst/coinList.js
+++ src/component/CoinLIst/coinLIst/coinList.js
@@ -1,43 +1,9 @@
 import React ,{useState,useEffect} from 'react';
 import { Table } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './coinList.css'
 import {Link} from 'react-router-dom';
 import { Navbar } from '../../navbar/navbar';
 
-// export const CoinList=props=>(
-//     <Table striped bordered hover  ="dark" className="container">
-//         <thead>
-//         <tr>
-//             <th>Logo</th>
-//             <th>Name</th>
-//             <th>Symbol</th>
-//             <th>Current Price</th>
-//             <th>Market Cap</th>
-//             <th>Price Change 24h</th>
-            
-           
-//         </tr>
-//         </thead>
-//         <tbody>
-//          {props.currency.map(coin=>(
-//             <tr>
-//             <td><img alt='coins' src={coin.image} style={{width:"50px",height:"50px"}}></img></td>
-//             <td>
-//                 <Link to={`/${coin.id}`}>{coin.name}</Link>
-//             </td>
-//             <td>{coin.symbol}</td>
-//             <td>${coin.current_price}</td>
-//             <td>{coin.market_cap}</td>
-//             <td>{coin.price_change_24h}</td>
-//             </tr>
-
-//          ))}   
-        
-//         </tbody>
-        
-//     </Table>
-// )
 export function CoinsList(){
     useEffect(()=>{
         fetchCoins();
